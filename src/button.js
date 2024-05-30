fc6fbf611fa84b10aaddd275dcb79966f5ab95c9
Edgar Daniel L. Moso <tedismoso02@gmail.com>
2024-05-30 16:01:01 -0400

    saving

--- src/button.js
+++ src/button.js
@@ -1,50 +1,64 @@
 const buttonTemplate = document.createElement('template');
 buttonTemplate.innerHTML = /* html */ `
+<style>
+	/* :host pseudo-element only applies if the element is shadow root is attached */
+	:host {
+		display: block;
+		margin-bottom: 1rem;
+	}
+
+	:host([inprogress]) {
+		transform: scale(1.1);
+		transform-origin: top left;
+	}
+</style>
+
 <link rel="stylesheet" href="./src/button.css">
 <button class="btn"><slot>Button Text</slot></button>
 `;
 
 class Button extends HTMLElement {
     constructor() {
-      super(); // htmlelement's constructor?
-      this.attachShadow({mode: "open"});
+		super(); // htmlelement's constructor?
+		this.attachShadow({mode: "open"});
     }
 
     connectedCallback() {
-      
-      this.shadowRoot.appendChild(buttonTemplate.content.cloneNode(true))
-      this.button = this.shadowRoot.querySelector('button');
-      this.initialValue = this.innerHTML;
+		this.shadowRoot.appendChild(buttonTemplate.content.cloneNode(true))
+		this.button = this.shadowRoot.querySelector('button');
+		this.initialValue = this.innerHTML;
     }
 
     set inprogress(progress) {
-      if(progress) {
-        this.setAttribute('inprogress', 'true');
-      } else {
-        this.removeAttribute('inprogress');
-      }
+		if(progress) {
+			this.setAttribute('inprogress', 'true');
+		} else {
+			this.removeAttribute('inprogress');
+		}
     }
 
     get inprogress() {
-      return this.getAttribute('inprogress');
+      	return this.getAttribute('inprogress');
     }
 
     static get observedAttributes() {
-      return ["inprogress"];
+      	return ["inprogress"];
     }
 
     attributeChangedCallback(attribute, oldValue, newValue) {
-      // const button = this.querySelector('button');
-      if (newValue) {
-        this.innerHTML = "Loading...";
-        this.button.setAttribute('disabled', 'true');
-        this.button.classList.add('fading');
-      } else {
-        this.innerHTML = this.initialValue;
-        this.button.removeAttribute('disabled');
-        this.button.classList.remove('fading');
-      }
+		// const button = this.querySelector('button');
+		if (newValue) {
+			this.innerHTML = "Loading...";
+			this.button.setAttribute('disabled', 'true');
+			this.button.classList.add('fading');
+		} else {
+			this.innerHTML = this.initialValue;
+			this.button.removeAttribute('disabled');
+			this.button.classList.remove('fading');
+		}
     }
   }
 
-  customElements.define('app-button', Button);
+setTimeout(() => {
+    customElements.define('app-button', Button);
+}, 2000)
--- src/card.js
+++ src/card.js
@@ -1,28 +1,51 @@
 const cardTemplate = document.createElement('template');
 cardTemplate.innerHTML = /* html */ `
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <style>
+        /* 
+            ::slotted selector are only applied to top level elements that are being slotted to the component
+            FOR EXAMPLE:
+            <div slot="card-header">
+                <h1>Header</h1>
+            </div>
+
+            the <div> element will only be targeted, not the <h1>
+        */
+
+        /*
+        ::slotted(h1) {
+            color: black!important;
+        }
+        */
+
         .card {
-            border: 1px solid black;
+            border-radius: 5px;
+            border: 1px solid var(--line-color, darkgray);
         }
 
-        .card__header,
-        .card__body {
-            border: 1px solid black;
+        .card-header {
+            padding: 5px;
+            background-color: var(--element-bg-color,  lightgray);
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+            border-bottom: 1px solid var(--line-color, darkgray);
+        }
+
+        .card-body {
+            padding: 5px;
         }
     </style>
 
     <div class="card">
-        <div class="card__header">
-            <slot name="card-header">Card Header</slot>
+        <div class="card-header">
+            <!-- Card header will be here -->
+            <slot name="card-header">
+                <h1>Card Header</h1>
+            </slot>
         </div>
 
-        <div class="card__body">
+        <div class="card-body">
             <slot name="card-body">Card Body</slot>
-        </div>
-
-        <div class="card__other">
-            <p>Other content goes here...</p>
-            <slot></slot>
         </div>
     </div>
 `
@@ -38,4 +61,6 @@ class Card extends HTMLElement {
     }
 }
 
-customElements.define('app-card', Card);
+setTimeout(() => {
+    customElements.define('app-card', Card);
+}, 2000)
